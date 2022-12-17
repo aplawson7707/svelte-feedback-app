535597c52d465354908465999b0beeef4d004e90
Alex Lawson <aplawson7707@gmail.com>
2022-12-17 01:40:10 -0700

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import FeedbackStats from "./components/FeedbackStats.svelte";
+	import FeedbackList from "./components/FeedbackList.svelte";
+    import FeedbackForm from "./components/FeedbackForm.svelte";
+</script>
+
+<main class="container">
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
+</main>
+
+<style>
+	
+</style>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {FeedbackStore} from '../stores'
+    import { scale } from 'svelte/transition'
+    import FeedbackItem from "./FeedbackItem.svelte"
+    
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+<div in:scale out:scale>
+    <FeedbackItem item={fb} />  
+</div>
+{/each}
